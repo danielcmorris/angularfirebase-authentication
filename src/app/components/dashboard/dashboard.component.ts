35b6a8b984d3982f2181905bc11ea8f00c6c3a3e
Daniel Morris <dmorris@morrisdev.com>
2021-11-01 18:55:44 -0700

    got firebase to work

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { AuthService } from "../../shared/services/auth.service";
 import { Router } from "@angular/router";
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { FirebaseDatabase, FirebaseFirestore, FirebaseFunctions } from '@angular/fire';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -10,12 +14,42 @@ import { Router } from "@angular/router";
 })
 export class DashboardComponent implements OnInit {
 
+  squares;
+  dblist: AngularFirestoreCollection<any>;
+
   constructor(
     public authService: AuthService,
+    public afs: AngularFirestore,   // Inject Firestore service
     public router: Router,
     public ngZone: NgZone
-  ) { }
+  ) {
+    this.dblist = this.afs.collection<any>('squares', ref => {
+      return ref.where("game", "==", 1)
+    });
 
-  ngOnInit() { }
+     this.dblist.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          console.log('data', data)
+          let retval = JSON.parse(JSON.stringify(data));
+          this.squares=retval;
+          return { retval };
+        });
+      })).subscribe();
+
+ 
+
+  }
+
+  ngOnInit() {
+
+  }
+
+
+  getSquare() {
+
+    //const db = getFirestore(app);
+  }
 
 }
